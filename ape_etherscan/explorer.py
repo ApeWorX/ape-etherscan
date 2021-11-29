7c557d3a35c0a57ae424d00e0e8d2d2b95282ad4
sabotage beats <27985126+sabotagebeats@users.noreply.github.com>
2021-11-29 14:47:13 -0800

    style: linting

--- ape_etherscan/explorer.py
+++ ape_etherscan/explorer.py
@@ -9,9 +9,10 @@ ETHERSCAN_URI = (
     + "{0}/{1}/"
 )
 
-ETHERSCAN_API_URI = (
-    lambda n: (f"https://api-{n}.etherscan.io/api" if n != "mainnet" else "https://api.etherscan.io/api")
+ETHERSCAN_API_URI = lambda n: (
+    f"https://api-{n}.etherscan.io/api" if n != "mainnet" else "https://api.etherscan.io/api"
 )
+
 
 class Etherscan(ExplorerAPI):
     def get_address_url(self, address: str) -> str:
@@ -21,8 +22,11 @@ class Etherscan(ExplorerAPI):
         return ETHERSCAN_URI(self.network.name).format("tx", transaction_hash)
 
     def get_contract_abi(self, address: str) -> List[ABI]:
-        response = requests.get(ETHERSCAN_API_URI(self.network.name),params={"module":"contract","action":"getabi","address":address})
-        abi_string = response.json()['result']
+        response = requests.get(
+            ETHERSCAN_API_URI(self.network.name),
+            params={"module": "contract", "action": "getabi", "address": address},
+        )
+        abi_string = response.json()["result"]
         return [ABI.from_dict(item) for item in json.loads(abi_string)]
-        #needs to return ContractType
-        #return ContractType.from_dict(item) for item in json.loads(abi_string)
+        # needs to return ContractType
+        # return ContractType.from_dict(item) for item in json.loads(abi_string)
