84eb9dd2f4ed134245514851ebf3e35841ba7db5
sabotage beats <27985126+sabotagebeats@users.noreply.github.com>
2021-12-01 12:46:35 -0800

    feat: added error handling

--- ape_etherscan/explorer.py
+++ ape_etherscan/explorer.py
@@ -1,6 +1,7 @@
+from json.decoder import JSONDecodeError
 from ape.api import ExplorerAPI
 from ape.types import ABI, ContractType
-from typing import List
+from typing import List, Optional
 import requests
 import json
 
@@ -20,13 +21,21 @@ class Etherscan(ExplorerAPI):
     def get_transaction_url(self, transaction_hash: str) -> str:
         return ETHERSCAN_URI(self.network.name).format("tx", transaction_hash)
 
-    def get_contract_type(self, address: str) -> List[ABI]:
+    def get_contract_type(self, address: str) -> Optional[ContractType]:
         response = requests.get(
             get_etherscan_uri(self.network.name),
             params={"module": "contract", "action": "getsourcecode", "address": address},
         )
-        print(response.raise_for_status())
-        abi_string = response.json()["result"][0]["ABI"]
-        abi = [ABI.from_dict(item) for item in json.loads(abi_string)]
-        contractName = response.json()["result"][0]["ContractName"]
+        result = response.json().get("result")
+        if not result or len(result) != 1:
+            return None
+        abi_string = result[0].get("ABI")
+        if not abi_string:
+            return None
+        try:
+            abi_list = json.loads(abi_string)
+        except JSONDecodeError:
+            return None
+        abi = [ABI.from_dict(item) for item in abi_list]
+        contractName = result[0].get("ContractName","unknown")
         return ContractType(abi=abi, contractName=contractName)
