480c2b178a14f466cc6fb7a2de5b54f4e77300f6
unparalleled-js <yingthi@live.com>
2022-01-07 11:38:41 -0600

    refactor: better response error

--- ape_etherscan/client.py
+++ ape_etherscan/client.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Optional
 import requests
 from ape.exceptions import ApeException
 from ape.utils import USER_AGENT
+from requests import Response
 
 
 def get_etherscan_uri(network_name: str):
@@ -39,9 +40,12 @@ class _APIClient:
         response = requests.get(self.base_uri, params=params, headers=headers)
         response.raise_for_status()
         response_data = response.json()
+        if response_data.get("isError", 0) or response_data.get("message", "") == "NOTOK":
+            raise ResponseError(response)
+
         result = response_data.get("result")
         if not result:
-            raise ResponseError(response.text)
+            raise ResponseError(response)
 
         return result
 
@@ -57,18 +61,27 @@ class ContractClient(_APIClient):
         return result[0] if len(result) == 1 else None
 
 
-class ResponseError(ApeException):
-    """
-    Raised when the response is not correct.
-    """
-
-    def __init__(self, response_text: str):
-        super().__init__(f"Response is not expected:\n{response_text}")
-
-
 class ClientFactory:
     def __init__(self, network_name: str):
         self._network_name = network_name
 
     def get_contract_client(self, contract_address: str) -> ContractClient:
         return ContractClient(self._network_name, contract_address)
+
+
+class ResponseError(ApeException):
+    """
+    Raised when the response is not correct.
+    """
+
+    def __init__(self, response: Response):
+        self.response = response
+        response_data = response.json()
+        if "result" in response_data:
+            message = response_data["result"]
+        elif "message" in response_data:
+            message = response_data["message"]
+        else:
+            message = response.text
+
+        super().__init__(f"Response indicated failure: {message}")
