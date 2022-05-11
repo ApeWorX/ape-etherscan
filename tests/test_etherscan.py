148fccb68b73a415780fb664a7bb8a509ac73702
unparalleled-js <yingthi@live.com>
2022-05-10 20:05:47 -0500

    test: rm unneeded test arg

--- tests/test_etherscan.py
+++ tests/test_etherscan.py
@@ -74,18 +74,18 @@ def setup_mock_get(mocker, etherscan_abi_response, expected_params):
 
 
 @pytest.mark.parametrize(
-    "ecosystem,network,expected_prefix,address",
+    "ecosystem,network,expected_prefix",
     [
-        ("ethereum", NETWORKS["ethereum"][0], "etherscan.io", ADDRESS),
-        ("ethereum", NETWORKS["ethereum"][1], "ropsten.etherscan.io", ADDRESS),
-        ("fantom", NETWORKS["fantom"][0], "ftmscan.com", ADDRESS),
-        ("fantom", NETWORKS["fantom"][1], "testnet.ftmscan.com", ADDRESS),
+        ("ethereum", NETWORKS["ethereum"][0], "etherscan.io"),
+        ("ethereum", NETWORKS["ethereum"][1], "ropsten.etherscan.io"),
+        ("fantom", NETWORKS["fantom"][0], "ftmscan.com"),
+        ("fantom", NETWORKS["fantom"][1], "testnet.ftmscan.com"),
     ],
 )
-def test_get_address_url(ecosystem, network, expected_prefix, address):
+def test_get_address_url(ecosystem, network, expected_prefix):
     expected = f"https://{expected_prefix}/address/{ADDRESS}"
     explorer = get_explorer(ecosystem, network)
-    actual = explorer.get_address_url(address)  # type: ignore
+    actual = explorer.get_address_url(ADDRESS)  # type: ignore
     assert actual == expected
 
 
