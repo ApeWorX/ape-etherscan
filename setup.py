9862e28144a2ac8d3ab8504670cc247b5359b613
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-10-12 15:31:06 -0500

    chore: bump ape core 0.5.2

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     url="https://github.com/ApeWorX/ape-etherscan",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.5.1,<0.6",
+        "eth-ape>=0.5.2,<0.6",
         "requests",  # Use same version as eth-ape
     ],
     python_requires=">=3.8,<3.11",
