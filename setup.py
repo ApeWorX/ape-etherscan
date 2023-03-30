46cc2e5b2d43b50321e6e870ea4311c3f3273145
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2023-03-30 18:14:56 -0400

    refactor: update pin to Ape v0.6.7

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     url="https://github.com/ApeWorX/ape-etherscan",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.6.6,<0.7",
+        "eth-ape>=0.6.7,<0.7",
         "requests",  # Use same version as eth-ape
     ],
     python_requires=">=3.8,<3.11",
