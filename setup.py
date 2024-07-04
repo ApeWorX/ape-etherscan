97648931b8d0f89697368dcff98d87a3dddf5899
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-07-04 08:16:12 +0200

    chore: uncomment ape-scroll

--- setup.py
+++ setup.py
@@ -12,9 +12,7 @@ extras_require = {
         "ape-optimism",  # Needed for Optimism integration
         "ape-polygon",  # Needed for Polygon integration
         "ape-polygon-zkevm",  # Needed for Polygon ZkEVM integration
-        # "ape-scroll",  # Needed for Scroll networks integration
-        # Temporary fix due to version restrictions
-        "ape-scroll @ git+https://github.com/Barabazs/ape-scroll.git@chore/version-restriction",
+        "ape-scroll",  # Needed for Scroll networks integration
         "ape-infura",  # Needed for live network tests
         "ape-solidity",  # Needed for contract verification tests
         "pytest>=6.0",  # Core testing package
