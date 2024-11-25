48fc1903ce0269a3868379bf98a8b09c8d2114e3
antazoey <admin@antazoey.me>
2024-11-25 09:14:37 -0600

    chore: Update setup.py
    Co-authored-by: slush <sam.loesch@gmail.com>

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ extras_require = {
         "flake8>=7.1.1,<8",  # Style linter
         "flake8-breakpoint>=1.1.0,<2",  # Detect breakpoints left in code
         "flake8-print>=5.0.0,<6",  # Detect print statements left in code
+        "flake8-pydantic",  # For detecting issues with Pydantic models
         "flake8-type-checking",  # Detect imports to move in/out of type-checking blocks
         "isort>=5.13.2,<6",  # Import sorting linter
         "mdformat>=0.7.19",  # Auto-formatter for markdown
